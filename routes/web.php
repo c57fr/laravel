52033fd0573bd9108d79dc2f7d1c8503224ff039
Lionel COTE <grcote7@gmail.com>
2017-04-03 06:41:15 +0200

    Reprise étude des routes

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,24 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
-    
-    Route::get('ok', function () {
-        return 'Oki';
-    });
-    
-    Route::get('', function () {
-        return 'Ajouter "/ok" à l\'URL';
-    });
-    
+
+  Route::get('ok', function () {
+    return 'Oki';
+  });
+
+  Route::get('', function () {
+    return 'Ajouter "/ok" à l\'URL';
+  });
+
 });
 
 
 Route::get('salut', function () {
-    return 'Salut les gens ! (GA)';
+  return 'Salut les gens ! (GA)';
 });
 
 // Fonctionne   http://laravel/salut/slug-marc-1
@@ -40,16 +40,16 @@ Route::get('salut/slug-{name}-{id}', function ($slug, $id) {
 Maintenant, essai sans slug dans la route
 */
 Route::get('salut/{slug}-{id}', ['as' => 'salut', function ($slug, $id) {
-    //  return ('Lien: /salut/' . $slug . '-' . $id );
-    
-    // Comme route nommée avec as:
-    return ('Lien: ' . route('salut', ['slug' => 'le-lien-est-' . $slug, 'id' => $id]));
-    
+  //  return ('Lien: /salut/' . $slug . '-' . $id );
+
+  // Comme route nommée avec as:
+  return ('Lien: ' . route('salut', ['slug' => 'le-lien-est-' . $slug, 'id' => $id]));
+
 }])->where('slug', '[a-z0-9\-]+')->where('id', '[0-9]+');
 
 
 Route::get('salut/{name}', function ($name) {
-    return ('Salut, ' . ucfirst($name) . ' !');
+  return ('Salut, ' . ucfirst($name) . ' !');
 });
 
 
@@ -65,6 +65,22 @@ Route::get('salut/{name}', function ($name) {
 
 // Autre que get: post, put, delete
 
-Route::get('test', function () {
-    return 'Je suis une page de test';
-})-> name ('test');
+Route::get('test1', function () {
+  //    return 'Je suis une page de test'; // Retourne une chaîne
+  //    return [1,2,3]; // Retourne du JSON
+  //  return response('un test', 206)->header('Content-Type', 'text/plain'); // Retourne un code partiel
+  return view('vue1');
+})->name('test');
+
+// Différentes façons d'envoyer un paramètre à une vue
+
+Route::get('artcile/{n}', function ($n) {
+  //  return view('documents/article')->with('numero', $n);
+  //  return view('documents/article')->withNumero($n);
+  return view('documents/article', ['numero' => $n]);
+})->where('n', '[0-9]+');
+
+
+Route::get('facture/{n}', function ($n) {
+  return view('documents/facture')->withNumero($n);
+})->where('n', '[0-9]+');
