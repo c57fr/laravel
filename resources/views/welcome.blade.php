6da9786e3fcf305392feaa7ffc47d060b0d89731
GrCOTE7 <GrCOTE7@Gmail.com>
2017-04-22 14:43:42 +0200

    Ajout code Piwik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -156,5 +156,29 @@
     </div>
 </div>
 
+<!-- Piwik -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+    _paq.push(["setCookieDomain", "*.c57.fr"]);
+    _paq.push(["setDomains", ["*.c57.fr"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//piwik.cote7.com/";
+        _paq.push(['setTrackerUrl', u + 'piwik.php']);
+        _paq.push(['setSiteId', '7']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.type = 'text/javascript';
+        g.async = true;
+        g.defer = true;
+        g.src = u + 'piwik.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<noscript><p><img src="//piwik.cote7.com/piwik.php?idsite=7&rec=1" style="border:0;" alt=""/></p></noscript>
+<!-- End Piwik Code -->
+
 </body>
 </html>
