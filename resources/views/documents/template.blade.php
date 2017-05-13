1e920532922db286677341a73b43e92bea49f67e
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-13 17:39:12 +0200

    GGA

--- resources/views/documents/template.blade.php
+++ resources/views/documents/template.blade.php
@@ -21,7 +21,6 @@ Partie commune du template
     a.src = g;
     m.parentNode.insertBefore(a, m)
   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
   ga('create', 'UA-61289532-2', 'auto');
   ga('send', 'pageview');
 </script>
